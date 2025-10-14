9c54750f0f9e6c5d9902868f2d120d867b87eb7f
venumuthusamy <venumuthusamy@gmail.com>
2025-10-14 12:48:02 +0530

    stack transfer

---
+++ starter-kit/src/Urls/StockTransferApiUrls.ts
@@ -0,0 +1,7 @@
+export class StockTransferApiUrls {
+  public static readonly CreateStockTransfer = "/StockTransfer/insert";
+  public static readonly GetAllStockTransfer = "/StockTransfer/getAll";
+  public static readonly GetStockTransferById = "/StockTransfer/get/"; 
+  public static readonly UpdateStockTransfer = "/StockTransfer/update";
+  public static readonly DeleteStockTransfer = "/StockTransfer/Delete/";
+}
