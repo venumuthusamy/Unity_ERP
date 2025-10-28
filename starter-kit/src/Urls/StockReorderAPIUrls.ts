d579bb92e54ecc42e0d751e4730bebde84593227
venumuthusamy <venumuthusamy@gmail.com>
2025-10-28 13:52:26 +0530

    stock reorder changes

---
+++ starter-kit/src/Urls/StockReorderAPIUrls.ts
@@ -0,0 +1,8 @@
+export class StockReorderApiUrls {
+  public static readonly CreateStockReorder = "/StockReorder/insert";
+  public static readonly GetAllStockReorder = "/StockReorder/getAll";
+  public static readonly GetAllWarehouseItems = "/StockReorder/warehouse-items";
+  public static readonly GetStockReorderById = "/StockReorder/get/"; 
+  public static readonly UpdateStockReorder = "/StockReorder/update";
+  public static readonly DeleteStockReorder = "/StockReorder/Delete/";
+}
