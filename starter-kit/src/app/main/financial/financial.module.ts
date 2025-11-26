e87f7f6c8bd472bd5708e457ad73c1c95b4eb807
Gowtham T <kuttygowtham756@gmail.com>
2025-11-26 10:08:51 +0530

    Changes

--- starter-kit/src/app/main/financial/financial.module.ts
+++ starter-kit/src/app/main/financial/financial.module.ts
@@ -66,7 +66,7 @@ const routes: Routes = [
     GeneralLdegerComponent,
     AgingComponent,
     PeriodCloseFxComponent,
-    InvoiceEmailComponent
+    InvoiceEmailComponent,
     TrialBalanceReportComponent
   ],
   imports: [
