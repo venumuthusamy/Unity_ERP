7aa21d7e22c6561df0f65ef7743d842157cb3f3c
Gowtham T <kuttygowtham756@gmail.com>
2025-12-16 07:04:59 +0530

    Changes

--- starter-kit/src/environments/environment.ts
+++ starter-kit/src/environments/environment.ts
@@ -5,8 +5,9 @@
 export const environment = {
   production: false,
   hmr: false,
-  apiUrl: 'http://localhost:7182/api'
+   apiUrl: 'http://localhost:7182/api'
  // apiUrl: 'http://localhost:5000/api'
+  //apiUrl: 'http://154.61.75.4:8085/api'
 };
 
 /*
